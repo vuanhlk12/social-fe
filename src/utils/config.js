6ae95f09d9b8160c6aff7ba275153ece10e89384
Bùi Vũ Anh <43056421+vuanhlk12@users.noreply.github.com>
2022-03-03 20:35:12 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@ const config = import.meta.env.PROD
       URL: "https://social-be-vuanh.herokuapp.com/",
       PUBLIC_FOLDER:
         "https://firebasestorage.googleapis.com/v0/b/social-img-17ab9.appspot.com/o/images/",
-      SOCKET_URL: "ws://https://social-socket-vuanh.herokuapp.com/",
+      SOCKET_URL: "ws://social-socket-vuanh.herokuapp.com",
     }
   : {
       URL: "http://localhost:8800/",
